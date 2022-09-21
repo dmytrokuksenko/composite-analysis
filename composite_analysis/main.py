5b2128a593ab79d6190a18d6b3cc882d84ad0a4b
unknown <kuksenko.dima@gmail.com>
2022-09-21 13:41:31 -0600

    added menu to the main window

--- composite_analysis/main.py
+++ composite_analysis/main.py
@@ -5,16 +5,13 @@
     @date: Sept 21, 2022
 """
 
-import numpy as np
 import sys
 import PyQt6.QtWidgets as QtWidets
 
 from mainwindow import MainWindow
-
 
 if __name__ == "__main__":
     app = QtWidets.QApplication(sys.argv)
     main = MainWindow()
     main.show()
     sys.exit(app.exec())
-
--- composite_analysis/mainwindow.py
+++ composite_analysis/mainwindow.py
@@ -2,7 +2,7 @@
     Main window of the graphical user interface (GUI).
 
     @author: Dmytro Kuksenko
-    @date: Sept 21, 2022 
+    @date: Sept 21, 2022
 """
 
 import PyQt6.QtWidgets as QtWidgets
@@ -12,3 +12,12 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Desing & Simulation of Composite Materials")
+
+        menu = self.menuBar()
+
+        file_menu = menu.addMenu("&File")
+        laminate_analysis = menu.addMenu("&Laminate Analysis")
+        filament_winding = menu.addMenu("&Filament Winding")
+        report = menu.addMenu("&Report")
+        help = menu.addMenu("&Help")
+        about = menu.addMenu("&About")
