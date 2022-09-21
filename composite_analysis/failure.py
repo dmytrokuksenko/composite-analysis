475eedc1a7f6d0de667bcb28f526e6d09f90fa4f
unknown <kuksenko.dima@gmail.com>
2022-09-21 15:35:54 -0600

    added a failure analysis template

---
+++ composite_analysis/failure.py
@@ -0,0 +1,34 @@
+"""
+    A collection of failure criteria for materials.
+
+    @author: Dmytro Kuksneko
+    @date: Sept 21, 2022
+"""
+
+
+def max_strain():
+    pass
+
+
+def max_stress():
+    pass
+
+
+def tsai_wu():
+    pass
+
+
+def tsai_hill():
+    pass
+
+
+def larc04():
+    pass
+
+
+def hashin():
+    pass
+
+
+def puck():
+    pass
