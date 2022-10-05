efed4067fd028681e47bf8bc8bf93bcc02fb5acf
Dmytro Kuksenko <kuksenko.dima@gmail.com>
2022-10-05 15:05:18 -0600

    added two tests to pytest

---
+++ tests/tests.py
@@ -0,0 +1,12 @@
+"""
+    Main tests for the composite analysis software.
+
+    @author: Dmytro Kuksenko
+    @date: Oct 5, 2022
+"""
+
+def test_always_pass():
+    assert True
+
+def test_always_fail():
+    assert False
