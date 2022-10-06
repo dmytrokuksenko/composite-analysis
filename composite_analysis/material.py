3329cac95e20c998c5fd68a07ebe83015d84da06
Dmytro Kuksenko <kuksenko.dima@gmail.com>
2022-10-06 14:08:00 -0600

    added classes for new materails + tests

--- composite_analysis/material.py
+++ composite_analysis/material.py
@@ -8,16 +8,22 @@
 
 class Material():
 
-    def __init__(self, name, props):
+    def __init__(self, name=None, props=None):
         self.name = name
         self.props = props
+    
+    def __len__(self):
+        return len(self.props)
 
-
-class Composite(Material):
+class Iso(Material):
     pass
 
-
-class Metal(Material):
+class TransOrtho(Material):
     pass
 
+class Ortho(Material):
+    pass
+
+class Aniso(Material):
+    pass
     
--- tests/tests.py
+++ tests/tests.py
@@ -5,13 +5,28 @@
     @date: Oct 5, 2022
 """
 from composite_analysis import __version__
-from composite_analysis.material import Composite, Metal
+from composite_analysis.material import Iso, TransOrtho, Ortho, Aniso
 
 def test_version():
+    """Tests a software version."""
     assert __version__ == "0.1.0"
 
-def test_new_composite_material():
-    assert Composite() is not None 
+def test_new_iso_mat():
+    """Tests a creation of a new isotropic material"""
+    assert Iso() is not None 
 
-def test_new_metal_material():
-    assert Composite() is not None 
+def test_new_trans_mat():
+    """Tests a creation of a new transversely isotropic material"""
+    assert TransOrtho() is not None
+
+def test_new_ortho_mat():
+    """Tests a creation of a new orthotropic material"""
+    assert Ortho() is not None
+
+def test_new_aniso_mat():
+    """Tests a creation of a new anisotropic material"""
+    assert Aniso() is not None
+
+def test_len_trans_mat():
+    """Tests a to"""
+    assert len(TransOrtho(props=[1,2,3,4,5])) == 5
